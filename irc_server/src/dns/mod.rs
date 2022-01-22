77b8aad90362fc11b67752c37b80df1baac6403b
Stephen Bennett <spb@libera.chat>
2022-01-22 21:57:47 +0000

    Check that forward DNS matches reverse before assigning hostnames, and also strip trailing dots

--- irc_server/src/dns/mod.rs
+++ irc_server/src/dns/mod.rs
@@ -35,10 +35,33 @@ impl DnsClient
         task::spawn(async move {
             let name = match resolver.reverse_lookup(addr).await
             {
-                Ok(lookup) => lookup.iter().next().map(|n| Hostname::convert(n).ok()).flatten(),
+                Ok(lookup) => {
+                    match lookup.iter().next() {
+                        Some(name) => {
+                            if Self::verify_forward_matches(&resolver, name, addr).await {
+                                Some(name.to_ascii())
+                            } else {
+                                None
+                            }
+                        },
+                        None => None
+                    }
+                },
                 Err(_) => None
             };
+            let name = name.map(|n| Hostname::convert(n.trim_end_matches('.')).ok()).flatten();
             let _res = chan.send(ConnectionEvent{source: conn_id, detail: EventDetail::DNSLookupFinished(name)}).await;
         });
+    }
+
+    async fn verify_forward_matches(resolver: &TokioAsyncResolver, name: &trust_dns_resolver::Name, addr: IpAddr) -> bool
+    {
+        match resolver.lookup_ip(name.clone()).await
+        {
+            Ok(lookup) => {
+                lookup.iter().any(|ip| ip == addr)
+            },
+            Err(_) => false
+        }
     }
 }
