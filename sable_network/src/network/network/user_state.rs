5698213ece6c06e2f2c17665962cba121869dd6d
TheDaemoness <she@daemoness.dev>
2022-07-30 14:30:38 -0700

    Fix emitting a nick change update w/o a nick change

--- sable_network/src/network/network/user_state.rs
+++ sable_network/src/network/network/user_state.rs
@@ -97,7 +97,7 @@ impl Network {
         }
     }
 
-    fn do_bind_nickname(&mut self, target: NicknameId, user: UserId, prev_nick: Nickname, event: &Event, updates: &dyn NetworkUpdateReceiver)
+    fn do_bind_nickname(&mut self, target: NicknameId, user: UserId, old_nick: Nickname, event: &Event, updates: &dyn NetworkUpdateReceiver)
     {
         if let Some(existing) = self.nick_bindings.remove(target.nick())
         {
@@ -111,7 +111,7 @@ impl Network {
             {
                 // The existing one wins. Collide the user attempting to bind,
                 // and put the existing binding back.
-                self.collide_user(user, prev_nick, event, updates);
+                self.collide_user(user, old_nick, event, updates);
                 self.nick_bindings.insert(existing.nick, existing);
                 return;
             }
@@ -127,14 +127,18 @@ impl Network {
         let new_binding = state::NickBinding::new(*target.nick(), user, event.timestamp, event.id);
         if let Some(user_object) = self.users.get(&user)
         {
-            let update = UserNickChange {
-                user: user_object.clone(),
-                old_nick: prev_nick,
-                new_nick: new_binding.nick
-            };
+            let new_nick = new_binding.nick;
+            self.nick_bindings.insert(new_nick, new_binding);
 
-            self.nick_bindings.insert(new_binding.nick, new_binding);
-            updates.notify(update, event);
+            // Emit UserNickChange update if a nick change happens as a result of this rebinding.
+            if old_nick != new_nick {
+                let update = UserNickChange {
+                    user: user_object.clone(),
+                    old_nick,
+                    new_nick
+                };
+                updates.notify(update, event);
+            }
         }
     }
 
