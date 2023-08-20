bb4786a32162c67514579b4d3d46f80acdc37877
Stephen Bennett <spb@libera.chat>
2023-08-20 17:55:59 +0100

    stop using deprecated timestamp method

--- sable_ircd/src/utils/time_utils.rs
+++ sable_ircd/src/utils/time_utils.rs
@@ -2,7 +2,7 @@ use chrono::prelude::*;
 
 pub fn format_timestamp(ts: i64) -> String
 {
-    Utc.timestamp(ts, 0).to_rfc3339_opts(SecondsFormat::Secs, true)
+    Utc.timestamp_opt(ts, 0).unwrap().to_rfc3339_opts(SecondsFormat::Secs, true)
 }
 
 pub fn parse_timestamp(str: &str) -> Option<i64>
