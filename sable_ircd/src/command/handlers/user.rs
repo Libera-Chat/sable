76328752f127a9cd3a34397ee6db0dd5b0f3e346
Val Lorentz <progval+git@progval.net>
2023-09-02 00:23:47 +0200

    USER: Forbid empty real names
    Collateral damage of 35b80ad3c18de9d83124fff9a53490d3e4ff4014.

--- sable_ircd/src/command/handlers/user.rs
+++ sable_ircd/src/command/handlers/user.rs
@@ -10,6 +10,14 @@ fn handle_user(
     _unused2: &str,
     realname: &str,
 ) -> CommandResult {
+    if realname.len() == 0 {
+        /* "The minimum length of <username> is 1, ie. it MUST NOT be empty. If it is
+         * empty, the server SHOULD reject the command with ERR_NEEDMOREPARAMS (even if
+         * an empty parameter is provided)"
+         * -- https://modern.ircdocs.horse/#user-message
+         */
+        return numeric_error!(NotEnoughParameters, "USER");
+    }
     // Ignore these results; they'll only fail if USER was already successfully processed
     // from this pre-client. If that happens we silently ignore the new values.
     source.user.set(Username::new_coerce(username)).ok();
