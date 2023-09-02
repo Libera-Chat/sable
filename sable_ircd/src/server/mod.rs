21d2f4f65f4ac436f0c5d57b18ef9e14c76d76bf
Val Lorentz <progval+git@progval.net>
2023-09-02 09:59:11 +0200

    Return ERROR command before closing connection due to connection error
    It's better than nothing, eg. when the error is 'I/O Error: stream did not contain valid UTF-8'

--- sable_ircd/src/server/mod.rs
+++ sable_ircd/src/server/mod.rs
@@ -261,6 +261,7 @@ impl ClientServer {
                             .await;
                         }
                     }
+                    conn.send(message::Error::new(&e.to_string()));
                 }
                 self.connections.write().remove(msg.source);
             }
