1eaf761f34493e129d595d53483f4bb7f5cfe3cd
Val Lorentz <progval+git@progval.net>
2023-09-21 21:47:06 +0200

    Send MOTD after connection registration
    > Upon successful completion of the registration process, the server MUST send,
    > in this order, the RPL_WELCOME (001) [...] The server SHOULD then respond as
    > though the client sent the LUSERS command and return the appropriate numerics.
    > If the user has client modes set on them automatically upon joining the network,
    > the server SHOULD send the client the RPL_UMODEIS (221) reply or a MODE message
    > with the client as target, preferably the former. [...] The server MUST then
    > respond as though the client sent it the MOTD command, i.e. it must send either
    > the successful Message of the Day numerics or the ERR_NOMOTD (422) numeric.
    
    -- https://modern.ircdocs.horse/#connection-registration
    
    Sable doesn't implement LUSERS yet so it's irrelevant; and the bit about
    user modes is a mistake, servers always send them after the MOTD.

--- sable_ircd/src/command/handlers/motd.rs
+++ sable_ircd/src/command/handlers/motd.rs
@@ -1,18 +1,11 @@
 use super::*;
 
 #[command_handler("MOTD")]
-fn handle_motd(server: &ClientServer, response: &dyn CommandResponse) -> CommandResult {
-    match &server.info_strings.motd {
-        None => response.numeric(make_numeric!(NoMotd)),
-        Some(motd) => {
-            response.numeric(make_numeric!(MotdStart, server.name()));
-            for ele in motd {
-                response.numeric(make_numeric!(Motd, ele))
-            }
-
-            response.numeric(make_numeric!(EndOfMotd));
-        }
-    }
-
+fn handle_motd(
+    server: &ClientServer,
+    response: &dyn CommandResponse,
+    source: UserSource,
+) -> CommandResult {
+    crate::utils::send_motd(server, response, &source)?;
     Ok(())
 }
