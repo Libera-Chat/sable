d19b109f3b1203723e2ada4e3ed060064732082a
Val Lorentz <progval+git@progval.net>
2024-04-19 16:57:02 +0200

    WHO: Add support for nick masks
    And some random rustfmt changes to `matchers.rs` because it was not
    imported before.

--- sable_network/src/policy/standard_user_policy.rs
+++ sable_network/src/policy/standard_user_policy.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::*;
 
 use UserPermissionError::*;
@@ -23,5 +25,31 @@ impl UserPolicyService for StandardUserPolicy {
 
     fn can_unset_umode(&self, _user: &wrapper::User, _mode: UserModeFlag) -> PermissionResult {
         Ok(())
+    }
+
+    fn can_list_user(&self, to_user: &User, user: &User) -> PermissionResult {
+        if !user.mode().has_mode(UserModeFlag::Invisible) {
+            return Ok(());
+        }
+
+        // If the target user is invisible, check whether they share any channel
+        let mut channels1: HashSet<_> = to_user
+            .channels()
+            .flat_map(|membership| membership.channel().map(|chan| chan.id()))
+            .collect();
+        let mut channels2: HashSet<_> = user
+            .channels()
+            .flat_map(|membership| membership.channel().map(|chan| chan.id()))
+            .collect();
+        if channels1.len() <= channels2.len() {
+            std::mem::swap(&mut channels1, &mut channels2);
+        }
+        for chan in channels2 {
+            if channels1.contains(&chan) {
+                return Ok(());
+            }
+        }
+
+        Err(PermissionError::User(Invisible))
     }
 }
--- sable_network/src/policy/user_policy.rs
+++ sable_network/src/policy/user_policy.rs
@@ -7,4 +7,7 @@ pub trait UserPolicyService {
     fn can_set_umode(&self, user: &wrapper::User, mode: UserModeFlag) -> PermissionResult;
     /// Determine whether a given user can unset a given user mode on themselves
     fn can_unset_umode(&self, user: &wrapper::User, mode: UserModeFlag) -> PermissionResult;
+    /// Determine whether one user can discover another without knowing their nick
+    /// (eg. with `WHO *`)
+    fn can_list_user(&self, touser: &User, user: &User) -> PermissionResult;
 }
